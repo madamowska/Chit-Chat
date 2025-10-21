e16112c395bb1e5c0e737928047314d7f9003fdf
madamowska <marta.adamowska.05@gmail.com>
2025-10-21 15:11:46 +0200

    project structure

---
+++ client/client.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"bufio"
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+	"time"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+
+	"a3/grpc/chitchat"
+)
+
+func main() {
+
+	conn, err := grpc.Dial("localhost:7000", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("did not work")
+	}
+	defer conn.Close()
+
+	client := chitchat.NewChitChatServiceClient(conn)
+
+	stream, err := client.Message(context.Background())
+	if err != nil {
+		log.Fatalf("did not work")
+	}
+
+	clientID := fmt.Sprintf("Client-%d", time.Now().UnixNano()) // i wanted to make the client ids sequential but it's more work so i just used random ids based on timestamp (so unique)
+	lamport := int64(0)
+
+	lamport++
+	stream.Send(&chitchat.ChatMessage{
+		ClientId:    clientID,
+		Content:     "",
+		LogicalTime: lamport,
+		Type:        chitchat.MessageType_CONNECT,
+	})
+
+	go func() {
+		for {
+			in, err := stream.Recv()
+			if err != nil {
+				log.Printf("Disconnected from server: %v", err)
+				return
+			}
+
+			// Lamport clock update (match server behavior)
+			if in.LogicalTime > lamport {
+				lamport = in.LogicalTime
+			}
+			lamport++
+
+			log.Printf("[%d] %s: %s", in.LogicalTime, in.ClientId, in.Content)
+		}
+	}()
+
+	go func() {
+		scanner := bufio.NewScanner(os.Stdin)
+		for scanner.Scan() {
+			text := strings.TrimSpace(scanner.Text())
+			if text == "" {
+				continue
+			}
+
+			lamport++
+			err := stream.Send(&chitchat.ChatMessage{
+				ClientId:    clientID,
+				Content:     text,
+				LogicalTime: lamport,
+				Type:        chitchat.MessageType_MESSAGE,
+			})
+			if err != nil {
+				log.Printf("Error sending message: %v", err)
+				return
+			}
+		}
+	}()
+	select {}
+
+}
---
+++ server/server.go
@@ -0,0 +1,114 @@
+package main
+
+import (
+	"log"
+	"net"
+
+	"a3/grpc/chitchat"
+
+	"google.golang.org/grpc"
+)
+
+type ChitChatServer struct {
+	chitchat.UnimplementedChitChatServiceServer
+
+	clients    map[string]chitchat.ChitChatService_MessageServer // map of all clients ()
+	connect    chan *chitchat.ChatMessage                        //channel for connects
+	disconnect chan *chitchat.ChatMessage                        //channel for disconnects
+	message    chan *chitchat.ChatMessage                        //channel for messages
+	lamport    int64                                             // lamport clock
+}
+
+func (s *ChitChatServer) Message(stream chitchat.ChitChatService_MessageServer) error {
+	var clientID string
+
+	for {
+		msg, err := stream.Recv()
+		if err != nil {
+			// Client disconnected
+			if clientID != "" {
+				s.disconnect <- &chitchat.ChatMessage{
+					ClientId: clientID,
+					Type:     chitchat.MessageType_DISCONNECT,
+				}
+			}
+			return nil
+		}
+
+		clientID = msg.ClientId
+
+		switch msg.Type {
+		case chitchat.MessageType_CONNECT:
+			s.clients[msg.ClientId] = stream
+			s.connect <- msg
+		case chitchat.MessageType_MESSAGE:
+			s.message <- msg
+		case chitchat.MessageType_DISCONNECT:
+			s.disconnect <- msg
+			return nil
+		}
+	}
+}
+
+func main() {
+
+	server := &ChitChatServer{
+		clients:    make(map[string]chitchat.ChitChatService_MessageServer),
+		connect:    make(chan *chitchat.ChatMessage),
+		disconnect: make(chan *chitchat.ChatMessage),
+		message:    make(chan *chitchat.ChatMessage),
+	}
+
+	go server.startBroadcastLoop()
+
+	grpcServer := grpc.NewServer()
+	listener, err := net.Listen("tcp", ":7000")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	chitchat.RegisterChitChatServiceServer(grpcServer, server)
+
+	err = grpcServer.Serve(listener)
+	if err != nil {
+		log.Fatalf("did not work: %v", err)
+	}
+	log.Println("Server started on port 7000")
+
+}
+
+func (s *ChitChatServer) startBroadcastLoop() {
+	for {
+		select {
+		case msg := <-s.connect:
+			s.lamport++
+			connectMsg := &chitchat.ChatMessage{
+				ClientId:    msg.ClientId,
+				Content:     "Participant " + msg.ClientId + " joined the conversation",
+				LogicalTime: s.lamport,
+				Type:        chitchat.MessageType_CONNECT,
+			}
+			s.broadcast(connectMsg)
+		case msg := <-s.message:
+			s.lamport++
+			s.broadcast(msg)
+		case msg := <-s.disconnect:
+			s.lamport++
+			disconnectMsg := &chitchat.ChatMessage{
+				ClientId:    msg.ClientId,
+				Content:     "Participant " + msg.ClientId + " left the conversation",
+				LogicalTime: s.lamport,
+				Type:        chitchat.MessageType_DISCONNECT,
+			}
+			s.broadcast(disconnectMsg)
+			delete(s.clients, msg.ClientId)
+		}
+	}
+}
+
+func (s *ChitChatServer) broadcast(msg *chitchat.ChatMessage) {
+	msg.LogicalTime = s.lamport
+	for _, clientStream := range s.clients {
+		clientStream.Send(msg)
+	}
+}
